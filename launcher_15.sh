7ceba2d27aba79953ce4c70c0ca82eea67ee96a7
“JiapengWu” <jiapeng.wu@mail.mcgill.ca>
2021-05-18 09:49:44 -0400

    added slurm script for computation specs

---
+++ launcher_15.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=rpp-bengioy
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=150G
+#SBATCH --mail-type=ALL
+#SBATCH --time=2-12:00:00
+#SBATCH --gres=gpu:1
+###########################
+
+bash
+source ~/.bashrc
+set -ex
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate VKGRNN
+echo $(date '+%Y_%m_%d_%H_%M') - $SLURM_JOB_NAME - $SLURM_JOBID - `hostname` >> ./rare_entity_machine_assignments.txt
+$@
